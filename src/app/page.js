15aaf1aa7436f72d90c7b5355b8c8241cf300df3
Lebowzz <100530737+Lebowzz@users.noreply.github.com>
2024-01-04 12:18:05 +0200

    mongoose-start-commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Message from "./Components/Message/Message";
-import MessagingPage from "./messaging/page";
-import SettingsPage from "./settings/page";
+import LoginPage from "./login/page";
 
 export default function Home() {
-  return <></>;
+  return <LoginPage />;
 }
